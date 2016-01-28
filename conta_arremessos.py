2f049ccb9b9c74f734d1d68b13bec71784821089
Otavio Carneiro <otavio@carneiro.blog.br>
2016-01-28 21:51:12 -0200

    Funciona!

--- conta_arremessos.py
+++ conta_arremessos.py
@@ -10,6 +10,16 @@ key = 0
 # ler stream da webcam
 captura = cv2.VideoCapture(0)
 
+# quantidade de arremessos
+arremessos = 0
+
+#linha verde
+altura_linha = 170
+
+visivel = False # a bola está na tela?
+abaixo = False  # abaixo da linha?
+acima = False   # ou acima dela?
+
 while key != 27 and key != 1048603: # tecla ESC no notebook e no desktop
     _, im = captura.read()
 
@@ -25,31 +35,40 @@ while key != 27 and key != 1048603: # tecla ESC no notebook e no desktop
                                              cv2.RETR_TREE,
                                              cv2.CHAIN_APPROX_SIMPLE)
 
-    # quantidade de objetos
-    objetos = 0
+    # desenha linha
+    cv2.line(im,
+             (0,altura_linha), (800,altura_linha),
+             (0,255,0), # cor verde
+             thickness=3)
 
     # retangulos
     for contorno in contornos:
         if cv2.contourArea(contorno) > 200: # considera apenas objetos grandes
-            objetos += 1
+            visivel = True
             ret_x, ret_y, ret_w, ret_h = cv2.boundingRect(contorno)
             cv2.rectangle(im, (ret_x,ret_y), 
                               (ret_x+ret_w, 
                                ret_y+ret_h),
                               (0,255,255),2)
 
+    # onde está a bola?
+    if visivel:
+        if ret_y < altura_linha:
+            acima = True
+            abaixo = False
+        else:
+            abaixo = True
+
+    if acima and abaixo:
+        arremessos += 1
+        abaixo = False
+        acima = False
+
     # mostra contagem
     cv2.putText(im,
-                '%02d' % objetos,
+                '%02d' % arremessos,
                 (500,70),
                 cv2.FONT_HERSHEY_TRIPLEX, 3, (255,0,0))
-
-    #linha verde
-    altura_linha = 170
-    cv2.line(im,
-             (0,altura_linha), (800,altura_linha),
-             (0,255,0), # cor verde
-             thickness=3)
 
     cv2.imshow('Resultado', im)
     # cv2.imshow('Resultado', resultado)
