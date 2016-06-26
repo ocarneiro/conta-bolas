600c804860520bb8564f07c3661f17755e2a817a
Otavio Carneiro <otavio@carneiro.blog.br>
2016-06-26 08:31:04 -0300

    new sliders for hsv max

--- new_approach.py
+++ new_approach.py
@@ -22,7 +22,7 @@ class ColorSlider(object):
     def __init__(self, init_value, color, position):
         self.value = init_value
         self.color = color
-        self.position = position
+        self.position = MARGIN_LEFT + position * DOT_SIZE
 
     def decrease(self):
         self.value -= INCREMENT
@@ -42,28 +42,36 @@ class Juggling(object):
     def __init__(self, capture):
         self.mirror_mode = True
         self.window_name = WINDOW_NAME
-        self.r = ColorSlider(20, RED, MARGIN_LEFT)
-        self.g = ColorSlider(20, GREEN, MARGIN_LEFT + DOT_SIZE)
-        self.b = ColorSlider(20, BLUE, MARGIN_LEFT + 2 * DOT_SIZE)
+        self.r = ColorSlider(20, RED, 0)
+        self.g = ColorSlider(20, GREEN, 1)
+        self.b = ColorSlider(20, BLUE, 2)
+        self.hmax = ColorSlider(20, RED, 3)
+        self.smax = ColorSlider(20, GREEN, 4)
+        self.vmax = ColorSlider(20, BLUE, 5)
         self.capture = capture
         self.get_feed()
         self.key_map = {
             113: self.r.decrease, 97:  self.r.increase, # red slider
             119: self.g.decrease, 115: self.g.increase, # green slider
-            101: self.b.decrease, 100: self.b.increase  # blue slider
-        }
+            101: self.b.decrease, 100: self.b.increase,  # blue slider
+            114: self.hmax.decrease, 102: self.hmax.increase,
+            116: self.smax.decrease, 103: self.smax.increase,
+            121: self.vmax.decrease, 104: self.vmax.increase
+         }
 
     def get_feed(self):
         _, self.image = self.capture.read()
         if self.mirror_mode:
             cv2.flip(self.image, 1, self.image)
 
-    def show(self):
+    def play(self):
         cv2.imshow(self.window_name, self.image)
 
     def act_on_key(self, key):
         if key in self.key_map:
             self.key_map[key]()
+        else:
+            print key
 
     def draw_slider(self, slider):
         cv2.circle(self.image, 
@@ -71,7 +79,8 @@ class Juggling(object):
                    DOT_SIZE/2, slider.color, FILLED)
 
     def draw_sliders(self):
-        for slider in (self.r, self.g, self.b):
+        # TODO create a list of sliders to iterate here
+        for slider in (self.r, self.g, self.b, self.hmax, self.smax, self.vmax):
             self.draw_slider(slider)
 
 
@@ -83,7 +92,7 @@ key = 0
 while key != 27 and key != 1048603:
     j.get_feed()
     j.draw_sliders()
-    j.show()
+    j.play()
     key = cv2.waitKey(10)
     if key >= 0:
         j.act_on_key(key)
